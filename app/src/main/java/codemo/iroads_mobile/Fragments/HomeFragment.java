2605be117d62324dedd403f388f7b4b4238fec05
PivithuruThejan <pivithuru.14@cse.mrt.ac.lk>
2018-03-28 20:28:48 +0530

    Nericell Reorientation done.

--- app/src/main/java/codemo/iroads_mobile/Fragments/HomeFragment.java
+++ app/src/main/java/codemo/iroads_mobile/Fragments/HomeFragment.java
@@ -8,26 +8,15 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import codemo.iroads_mobile.R;
+import codemo.iroads_mobile.Reorientation.NericellMechanism;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,20 +29,13 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     private SignalProcessor zValueSignalProcessor;
     private SignalProcessor yValueSignalProcessor;
     private SignalProcessor xValueSignalProcessor;
-    private boolean enableFilter;
-    private ArrayList<Double> dataArray;
-    private ArrayList<Double> timeArray;
-    private ArrayList<Location> locationArray;
-    private boolean previousDirection;
-    private static Location cuurentLoc;
-    private StringBuilder dataReport;
+    private NericellMechanism nericellMechanism;
 
     Sensor accelerometer;
     Sensor magnetometer;
     TextView xValue, xValueFiltered, yValue, yValueFiltered, zValue, zValueAverageFiltered,
-            zValueHighPassFiltered ;
+            zValueHighPassFiltered, zValueReoriented, yValueReoriented, xValueReoriented;
     TextView xMagValue, yMagValue, zMagValue;
-    Button saveBtn;
     static TextView  lat, lng;
     public HomeFragment() {
         // Required empty public constructor
@@ -74,21 +56,20 @@ public class HomeFragment extends Fragment implements SensorEventListener {
         this.zValueSignalProcessor = new SignalProcessor(); // creates filters for sensor values
         this.yValueSignalProcessor = new SignalProcessor();
         this.xValueSignalProcessor = new SignalProcessor();
-        dataArray = new ArrayList<Double>(); // create array for storing acceleration data
-        timeArray = new ArrayList<Double>(); // create array for storing time
-        locationArray = new ArrayList<Location>(); // create array for storing locations
-        previousDirection = true; // if positive
-        dataReport = new StringBuilder();
+        this.nericellMechanism = new NericellMechanism();
         if(accelerometer != null){
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
             xValue = (TextView) view.findViewById(R.id.xValue);
             xValueFiltered = (TextView) view.findViewById(R.id.xValueFiltered);
+            xValueReoriented = (TextView) view.findViewById(R.id.xValueReoriented);
             // filtering process for sensor values
             yValue = (TextView) view.findViewById(R.id.yValue);
             yValueFiltered = (TextView) view.findViewById(R.id.yValueFiltered);
+            yValueReoriented = (TextView) view.findViewById(R.id.yValueReoriented);
             zValue = (TextView) view.findViewById(R.id.zValue);
             zValueAverageFiltered = (TextView) view.findViewById(R.id.zValueAverageFiltered);
             zValueHighPassFiltered = (TextView) view.findViewById(R.id.zValueHighPassFiltered);
+            zValueReoriented = (TextView) view.findViewById(R.id.zValueReoriented);
         }else{
             Log.d(TAG, "Accelorometer not available");
         }
@@ -107,38 +88,6 @@ public class HomeFragment extends Fragment implements SensorEventListener {
         lat = (TextView) view.findViewById(R.id.lat);
         lng = (TextView) view.findViewById(R.id.lng);
 
-        saveBtn = (Button) view.findViewById(R.id.saveBtn);
-        saveBtn.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                writeLog("\n \n" + sdf.format(new Date()) + dataReport.toString() + "\n \n");
-                dataReport = new StringBuilder();
-            }
-        });
-
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//                writeLog("\n \n" + sdf.format(new Date()) + dataReport.toString() + "\n \n");
-//                dataReport = new StringBuilder();
-//                Log.d(TAG,"\n********************** File Writting *************\n");
-//            }
-//        }, 10000);
-
-//        new Timer().schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                // this code will be executed after 2 seconds
-//                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//                writeLog("\n \n" + sdf.format(new Date()) + dataReport.toString() + "\n \n");
-//                dataReport = new StringBuilder();
-//                Log.d(TAG,"\n********************** File Writting *************\n");
-//            }
-//        }, 10000);
-
         return view;
 
     }
@@ -155,25 +104,17 @@ public class HomeFragment extends Fragment implements SensorEventListener {
 
         if(sensorType == Sensor.TYPE_ACCELEROMETER){
             xValue.setText("X Value: "+sensorEvent.values[0]);
-            xValueFiltered.setText("X ValueFiltered: "+ this.xValueSignalProcessor.
-                    averageFilter(sensorEvent.values[0]));
+            xValueFiltered.setText("X ValueFiltered: "+ this.xValueSignalProcessor.averageFilter(sensorEvent.values[0]));
+            xValueReoriented.setText("X ValueReoriented: "+ this.nericellMechanism.reOrientX(sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]));
             yValue.setText("Y Value: "+sensorEvent.values[1]);
-            yValueFiltered.setText("Y ValueFiltered: "+ this.yValueSignalProcessor.
-                    averageFilter(sensorEvent.values[1]));
+            yValueFiltered.setText("Y ValueFiltered: "+ this.yValueSignalProcessor.averageFilter(sensorEvent.values[1]));
+            yValueReoriented.setText("Y ValueReoriented: "+ this.nericellMechanism.reOrientY(sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]));
             zValue.setText("Z Value: "+sensorEvent.values[2]);
             zValueAverageFiltered.setText("Z ValueAverageFiltered: "+ this.zValueSignalProcessor.
                     averageFilter(sensorEvent.values[2]));
             zValueHighPassFiltered.setText("Z ValueHighPassFiltered: "+ this.zValueSignalProcessor.
                     highPassFilter(sensorEvent.values[2]));
-            setEnableFilter(false);
-            if(cuurentLoc != null){
-                if(isEnableFilter()){
-                    processIRI(this.zValueSignalProcessor.averageFilter(sensorEvent.values[2]));
-                }else{
-                    processIRI(sensorEvent.values[2]);
-//                Log.d(TAG,"--------------- data --------- /// "+sensorEvent.values[2]);
-                }
-            }
+            zValueReoriented.setText("Z ValueReoriented: " + this.nericellMechanism.reorientZ(sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]));
         }else if(sensorType == Sensor.TYPE_MAGNETIC_FIELD){
             xMagValue.setText("X Value: "+sensorEvent.values[0]);
             yMagValue.setText("Y Value: "+sensorEvent.values[1]);
@@ -187,122 +128,7 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     }
 
     public static  void updateLocation(Location loc){
-        cuurentLoc = loc;
         lat.setText("Latitude: "+ loc.getLatitude());
         lng.setText("Longitude: "+ loc.getLongitude());
     }
-
-
-    public boolean isEnableFilter() {
-        return enableFilter;
-    }
-
-    public void setEnableFilter(boolean enableFilter) {
-        this.enableFilter = enableFilter;
-    }
-
-    public void processIRI(double z){
-//        boolean directionChanged = false;
-
-        boolean currentDirection = true;  // if positive
-        double earthGravity = 9.95;
-        double thresholdPos = 0.4;
-        double thresholdNeg = -0.4;
-        double accelerationDiff = z-earthGravity;
-        if(accelerationDiff > thresholdPos || accelerationDiff < thresholdNeg ){
-            Double time = Double.valueOf(System.currentTimeMillis());
-
-            Log.d(TAG,"--------------- time ---------"+time);
-
-            if(accelerationDiff > 0.0){
-                Log.d(TAG,"--------------- Accelleration POSITIVE Diff ---------  "+accelerationDiff);
-            }else{
-                Log.d(TAG,"++++++++++++++++ Accelleration NEGATIVE Diff ++++++++++  "+accelerationDiff);
-                currentDirection = false;
-            }
-            if(previousDirection != currentDirection){
-                if(dataArray.size() > 1){
-                    calculateArea();
-                }
-                dataArray.clear();
-                dataArray.add(accelerationDiff);
-                timeArray.add(time);
-                locationArray.add(cuurentLoc);
-            }else{
-                dataArray.add(accelerationDiff);
-                timeArray.add(time);
-            }
-            previousDirection = currentDirection;
-            dataReport.append("\nAcceleration Difference: "+accelerationDiff);
-            dataReport.append(" TimeStamp: "+time);
-            dataReport.append(" Location: "+cuurentLoc.getLatitude() + " " + cuurentLoc.getLongitude());
-
-        }else{
-            // do nothing
-        }
-    }
-
-    public void calculateArea(){
-        double sum= 0.0;
-        double totTime=0.0;
-        for (int i= 0; i<dataArray.size()-1; i++){
-            double deltaT = timeArray.get(i+1)-timeArray.get(i);
-            Log.d(TAG,"********************** Delta time *************" + deltaT);
-            if(deltaT < 145.0){
-                sum += (dataArray.get(i) + dataArray.get(i+1)) * deltaT;
-                totTime += deltaT;
-            }else{
-                if(sum != 0.0){
-                    double velocityDiff = sum / 2000;
-                    Log.d(TAG,"********************** Velocity Diff *************" + velocityDiff);
-                    double displacement = velocityDiff * totTime / 2;
-                    dataReport.append("\nDisplacement: " + displacement);
-                    Log.d(TAG,"********************** Displacement ********************************************************" + displacement);
-                    sum = 0.0;
-                    totTime = 0.0;
-                }
-            }
-        }
-        if(sum != 0.0){
-            double velocityDiff = sum / 2000;
-            Log.d(TAG,"********************** Velocity Diff *************" + velocityDiff);
-            double displacement = velocityDiff * totTime / 2;
-            dataReport.append("\nDisplacement: " + displacement);
-            Log.d(TAG,"********************** Displacement ********************************************************" + displacement);
-        }
-//        totTime = dataArray.get(dataArray.size()-1) - dataArray.get(0)
-
-
-    }
-
-    public void writeLog(String text)
-    {
-        File logFile = new File("sdcard/log.txt");
-        if (!logFile.exists())
-        {
-            try
-            {
-                logFile.createNewFile();
-            }
-            catch (IOException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        try
-        {
-            //BufferedWriter for performance, true to set append to file flag
-            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
-            buf.append(text);
-            buf.newLine();
-            buf.close();
-        }
-        catch (IOException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
 }
--- app/src/main/java/codemo/iroads_mobile/Reorientation/NericellMechanism.java
+++ app/src/main/java/codemo/iroads_mobile/Reorientation/NericellMechanism.java
@@ -3,16 +3,92 @@ package codemo.iroads_mobile.Reorientation;
 import android.widget.TextView;
 
 /**
- * Created by dushan on 3/18/18.
+ * Created by Pivithuru 26/03/2018.
  */
 
 public class NericellMechanism implements Reorientation{
+
+    //private double teta;
+    //private double pie;
+    private boolean stable = true;
 
     @Override
     public void reorient() {
 
         //accelerometer xyx
         TextView xValueA, yValueA, zValueA;
+
+    }
+
+    public double teta(double y){
+        double teta = Math.acos(y/9.800);
+        return teta;
+    }
+
+    public double pie(double x, double z){
+        double pie = Math.atan(z/x);
+        return pie;
+    }
+
+    public void setStable(boolean stable) {
+        this.stable = stable;
+    }
+
+    public double reOrientX (double x, double y, double z) {
+        double teta = 0;
+        double pie = 0;
+        if (this.stable) {
+            teta = this.teta(y);
+            pie = this.pie(x, z);
+            //this.setStable(false);
+        }
+        double xPie = x*Math.cos(pie) - z*Math.sin(pie);
+        double yPie = y;
+        double zPie = x*Math.sin(pie) + z*Math.cos(pie);
+        double xTeta = xPie*Math.cos(teta) + yPie*Math.sin(teta);
+        double zTeta = zPie;
+        double alpha = Math.atan(xPie/zPie);
+        double xAplha = xTeta*Math.cos(alpha) - zTeta*Math.sin(alpha);
+        return xAplha;
+    }
+
+    public double reOrientY (double x, double y, double z) {
+        double teta = 0;
+        double pie = 0;
+        if (this.stable) {
+            teta = this.teta(y);
+            pie = this.pie(x, z);
+            //this.setStable(false);
+        }
+        double xPie = x*Math.cos(pie) - z*Math.sin(pie);
+        double yPie = y;
+        double zPie = x*Math.sin(pie) + z*Math.cos(pie);
+        double xTeta = xPie*Math.cos(teta) + yPie*Math.sin(teta);
+        double yTeta = -xPie*Math.sin(teta) + yPie*Math.cos(teta);
+        double zTeta = zPie;
+        double alpha = Math.atan(xPie/zPie);
+        double yAlpha = yTeta;
+        return yTeta;
+
+    }
+
+    public double reorientZ (double x, double y, double z) {
+        double teta = 0;
+        double pie = 0;
+        if (this.stable) {
+            teta = this.teta(y);
+            pie = this.pie(x, z);
+            //this.setStable(false);
+        }
+        double xPie = x*Math.cos(pie) - z*Math.sin(pie);
+        double yPie = y;
+        double zPie = x*Math.sin(pie) + z*Math.cos(pie);
+        double xTeta = xPie*Math.cos(teta) + yPie*Math.sin(teta);
+        double yTeta = -xPie*Math.sin(teta) + yPie*Math.cos(teta);
+        double zTeta = zPie;
+        double alpha = Math.atan(xPie/zPie);
+        double zAlpha = xTeta*Math.sin(alpha) + zTeta*Math.cos(alpha);
+        return zAlpha;
 
     }
 }
