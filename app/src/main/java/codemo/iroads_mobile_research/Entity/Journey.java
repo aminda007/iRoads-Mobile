32a2ea3bfb89c721eedaa4817266896b98b7b89e
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-11-12 12:40:55 +0530

    add journey file

---
+++ app/src/main/java/codemo/iroads_mobile_research/Entity/Journey.java
@@ -0,0 +1,28 @@
+package codemo.iroads_mobile_research.Entity;
+
+public class Journey {
+
+    private static String name;
+    private static String id;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Journey.name = name;
+    }
+
+    public static String getId() {
+        return id;
+    }
+
+    public static void setId(String id) {
+        Journey.id = id;
+    }
+
+    public static void clear(){
+        Journey.name = null;
+        Journey.id = null;
+    }
+}
